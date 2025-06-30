40f6bcfd40345166c96c4f760c47c93e8f4103c9
Davi Felipe Zeneratto <163222160+davifz@users.noreply.github.com>
2025-06-30 20:15:40 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿alert("Seja bem-vindo ao nosso jogo!");
+
+let qtd;
+qtd = prompt("Digite um numero pra fazer a media: ");
+
+let i;
+let soma = 0;
+for (i = 1; i <= qtd; i++) {
+  soma = soma + i;
+}
+let media = soma / qtd;
+
+alert(`Sua média é: ${media}`);
