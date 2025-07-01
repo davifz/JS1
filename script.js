728ff6d7d762b35cfa961ce94e19fdbbcb37a671
Davi Felipe Zeneratto <163222160+davifz@users.noreply.github.com>
2025-07-01 00:59:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-alert("Bem-vindo ao Jogo da Média!");
-  let quantidade = prompt("Quantos números você quer digitar?");
-  quantidade = Number(quantidade);
-  if (quantidade <= 0 || quantidade === null || quantidade === "" || typeof quantidade !== "number") {
-    alert("Quantidade inválida. Tente novamente com um número maior que zero.");
-  } else {
-    let soma = 0;
+alert('Seja bem-vindo ao nosso jogo!');
+  let numeroSecreto = 6;
+  let chute;
+  let tentativas = 0;
+  while (chute != numeroSecreto) {
+    chute = parseInt(prompt('Escolha um número entre 1 e 10'));
+    tentativas++;
 
-    for (let i = 1; i <= quantidade; i++) {
-      let entrada = prompt(`Digite o ${i}º número:`);
-      let numero = Number(entrada);
-
-      if (entrada.trim() === "" || entrada === null || isNaN(numero)) {
-        alert("Valor inválido. Digite um número.");
-        i--;
-        continue;
-      }
-
-      soma += numero;
+    if (chute == numeroSecreto) {
+      alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} na tentativa ${tentativas}.`);
+    } else if (chute > numeroSecreto) {
+      alert(`O número secreto é menor que ${chute}. Tente novamente.`);
+    } else {
+      alert(`O número secreto é maior que ${chute}. Tente novamente.`);
     }
-
-    let media = soma / quantidade;
-
-    alert(`A média calculada entre os ${quantidade} números digitados é: ${media.toFixed(2)}`);
   }
+}
