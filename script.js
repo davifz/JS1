665c2f0e91c87621731ad0be2135948de571004e
Davi Felipe Zeneratto <163222160+davifz@users.noreply.github.com>
2025-07-01 01:31:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,11 @@
-alert("Bem-vindo ao Jogo da Média!");
-  let quantidade = prompt("Quantos números você quer digitar?");
-  quantidade = Number(quantidade);
-  if (quantidade <= 0 || quantidade === null || quantidade === "" || typeof quantidade !== "number") {
-    alert("Quantidade inválida. Tente novamente com um número maior que zero.");
-  } else {
-    let soma = 0;
+alert("Bem vindo ao jogo da média");
+let qtdNum = parseInt(prompt("Quantos números você gostaria de usar para calcular a média?"));
+let soma = 0;
 
-    for (let i = 1; i <= quantidade; i++) {
-      let entrada = prompt(`Digite o ${i}º número:`);
-      let numero = Number(entrada);
-
-      if (entrada.trim() === "" || entrada === null || isNaN(numero)) {
-        alert("Valor inválido. Digite um número.");
-        i--;
-        continue;
-      }
-
-      soma += numero;
-    }
-
-    let media = soma / quantidade;
-
-    alert(`A média calculada entre os ${quantidade} números digitados é: ${media.toFixed(2)}`);
-  }
+for (let i = 1; i <= qtdNum; i++) {
+  let num = parseInt(prompt(`Digite o ${i}º número:`));
+  soma += num;
 }
+
+let media = soma / qtdNum;
+alert(`A média dos ${qtdNum} números é igual a ${media}`);
